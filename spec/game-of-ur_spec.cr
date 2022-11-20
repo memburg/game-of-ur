6c1ba4776a308313aeea6b118e6dbb83a2df0a0f
Ramb Memburg <ramb.memburg@gmail.com>
2022-11-20 12:33:30 -0600

    big bang 💥

---
+++ spec/game-of-ur_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Game::Of::Ur do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/game-of-ur.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Game::Of::Ur`
+module Game::Of::Ur
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
