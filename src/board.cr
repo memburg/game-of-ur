458cb5a87c59b73e1b5f616becccda1b81ff4281
Ramb Memburg <ramb.memburg@gmail.com>
2022-11-26 19:02:50 -0600

    move runes

--- src/board.cr
+++ src/board.cr
@@ -3,25 +3,26 @@ require "./player.cr"
 
 class Board
   # Class constants
-  OCCUPIED      = "OC"
-  AVAILABLE     = "AV"
-  NOT_AVAILABLE = "NA"
+  FREE       = "FR"
+  OCCUPIED   = "OC"
+  RESTRICTED = "RE"
 
   # Initialize class with 2 players (this game is
   # a game of 2 :P)
   def initialize
+    @moves = "P1"
     @player_a = Player.new
     @player_b = Player.new
     @board_map = { # Mapping of the board
-      "A" => [AVAILABLE, AVAILABLE, AVAILABLE, NOT_AVAILABLE, NOT_AVAILABLE, AVAILABLE, AVAILABLE],
-      "B" => [AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE],
-      "C" => [AVAILABLE, AVAILABLE, AVAILABLE, NOT_AVAILABLE, NOT_AVAILABLE, AVAILABLE, AVAILABLE],
+      "A" => [FREE, FREE, FREE, RESTRICTED, RESTRICTED, FREE, FREE],
+      "B" => [FREE, FREE, FREE, FREE, FREE, FREE, FREE],
+      "C" => [FREE, FREE, FREE, RESTRICTED, RESTRICTED, FREE, FREE],
     }
   end
 
   def print_board
-    puts "\33c\e[3J                                                 "
-    puts "LAST MOVEMENT: NA                                         "
+    puts "\33c\e[3JPLAYER A:                                        "
+    puts "A1, A2, A3, A4, A5, A6, A7                                "
     puts "                                                          "
     puts "     00     01     02     03     04     05     06     07  "
     puts "                                                          "
@@ -37,9 +38,29 @@ class Board
     puts "C  ░░░░░░ ░░░░░░ ░░░░░░ ░░░░░░               ░░░░░░ ░░░░░░"
     puts "   ░░░░░░ ░░░░░░ ░░░░░░ ░░░░░░               ░░░░░░ ░░░░░░"
     puts "                                                          "
-    puts "PLAYER A:                       PLAYER B:                 "
-    puts "A1, A2, A3, A4, A5, A6, A7      B1, B2, B3, B4, B5, B6, B7"
+    puts "PLAYER B:                                                 "
+    puts "B1, B2, B3, B4, B5, B6, B7                                "
 
-    @player_a.move_rune(1, "A01")
+    move_rune(@player_a, 1, "A00")
+  end
+
+  private def move_rune(p : Player, rune : Int32, position : String)
+    puts p.runes
+    row, middle, column = position.split("")
+
+    if row < "A" && row > "C"
+      puts "#{row} IS NOT A VALID ROW"
+      return
+    end
+
+    if column < "0" || column > "7"
+      puts "#{column} IS NOT A VALID COLUMN"
+      return
+    end
+
+    p.runes[rune - 1] = position
+
+    puts p.runes
+    pp @board_map
   end
 end
